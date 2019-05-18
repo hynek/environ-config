079644d378ac66f9c10746f99e30f68570989701
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2019-05-18 17:44:07 +0200

    Created config class got `from_environ` method (fix #2)

---
+++ tests/test_class_to_config.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function
+import os
+
+import attr
+import pytest
+
+import environ
+
+
+@environ.config(prefix="APP")
+class AppConfig(object):
+    host = environ.var("127.0.0.1")
+    port = environ.var(5000, converter=int)
+
+
+def test_default():
+    cfg = AppConfig.from_environ()
+    assert cfg.host == "127.0.0.1"
+    assert cfg.port == 5000
+
+
+def test_env():
+    env = {"APP_HOST": "0.0.0.0"}
+    cfg = AppConfig.from_environ(environ=env)
+    assert cfg.host == "0.0.0.0"
+    assert cfg.port == 5000
