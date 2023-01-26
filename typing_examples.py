0990e1d48849eabbcf1c1550f2ab6a69829330af
Hynek Schlawack <hs@ox.cx>
2023-01-26 09:21:53 +0100

    Add hints for bool_var & generate help

--- typing_examples.py
+++ typing_examples.py
@@ -8,8 +8,11 @@ class Config:
         y: int = environ.var(converter=int)
 
     x: str = environ.var()
+    b: bool = environ.bool_var(name="BOOL")
     sub: Sub = environ.group(Sub)
 
+
+s: str = environ.generate_help(Config)
 
 cfg = environ.to_config(Config, {"APP_X": "123"})
 
@@ -23,4 +26,5 @@ def takes_sub(s: Config.Sub) -> int:
 
 
 x: str = takes_cfg(cfg)
+b: bool = cfg.b
 y: int = takes_sub(cfg.sub)
