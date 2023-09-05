02575eaf84278fb994e627d3cc4f0adb0bf8a462
Hynek Schlawack <hs@ox.cx>
2023-09-05 09:35:37 +0200

    Drop Python 3.7

--- src/environ/__init__.py
+++ src/environ/__init__.py
@@ -46,13 +46,9 @@ def __getattr__(name: str) -> str:
     if name not in dunder_to_metadata:
         raise AttributeError(f"module {__name__} has no attribute {name}")
 
-    import sys
     import warnings
 
-    if sys.version_info < (3, 8):
-        from importlib_metadata import metadata
-    else:
-        from importlib.metadata import metadata
+    from importlib.metadata import metadata
 
     warnings.warn(
         f"Accessing environ_config.{name} is deprecated and will be "
