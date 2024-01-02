c978ff9cf596cd41e04bfc1e718f0aa9a5d345f2
Hynek Schlawack <hs@ox.cx>
2024-01-02 11:19:06 +0100

    Use nicer fonts

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@ myst_enable_extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
 
+html_css_files = ["custom.css"]
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 source_suffix = [".rst", ".md"]
