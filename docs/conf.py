424413538eb0763516fc98beb16fbc167b90878c
Hynek Schlawack <hs@ox.cx>
2024-05-08 10:20:45 +0200

    Fix furo warning

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ todo_include_todos = False
 
 html_theme = "furo"
 html_theme_options = {
-    "top_of_page_button": None,
+    "top_of_page_buttons": [],
     "light_css_variables": {
         "font-stack": "Inter, sans-serif",
         "font-stack--monospace": "BerkeleyMono, MonoLisa, ui-monospace, "
